0a100cfe228972443df5dd25e6486528a5dd678c
Sedat MENGÜ <sedatmengu@gmail.com>
2023-03-30 18:21:03 +0300

    String Veri Tipi

---
+++ StringVeriTipi.py
@@ -0,0 +1,71 @@
+name = "HELLO WORLD!"
+
+
+print(name.capitalize())
+#print = Hello world! (Sadece kelimenin ilk harfi büyük olur)
+
+print(name.casefold())
+#print = hello world! ( bütün harfler küçük olur)
+
+
+print(name.center(20,"-"))
+#print = ----HELLO WORLD!---- ( string ifadeyi 20 karaktere tamamlayıp ifadeyi ortalar )
+
+
+print(name.count("L"))
+#print = 3 ( string ifade içerisinde kaç tane L harfi olduğunu sayar.)
+
+print(name.encode)
+
+print(name.endswith("!"))
+#print = true (ifadenin parantez içinde belirtilen ile mi bitip bitmediğini kontrol eder. sonuç true / false)
+
+print(name.expandtabs)
+print(name.find)
+print(name.format)
+print(name.format_map)
+print(name.index)
+print(name.isalnum)
+print(name.isalpha)
+print(name.isascii)
+print(name.isdecimal)
+print(name.isdigit)
+print(name.isidentifier)
+print(name.islower)
+print(name.isnumeric)
+print(name.isprintable)
+print(name.isspace)
+print(name.istitle)
+print(name.isupper)
+print(name.join)
+print(name.ljust)
+print(name.lower)
+print(name.lstrip)
+print(name.maketrans)
+print(name.partition)
+print(name.removeprefix)
+print(name.removesuffix)
+print(name.replace)
+print(name.rfind)
+print(name.rindex)
+print(name.rjust)
+print(name.rpartition)
+print(name.rsplit)
+print(name.rstrip)
+print(name.split)
+print(name.splitlines)
+print(name.startswith)
+print(name.strip)
+print(name.swapcase)
+print(name.title)
+print(name.translate)
+print(name.upper)
+print(name.zfill)
+
+
+
+
+
+
+
+
