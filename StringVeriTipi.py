366ef0d66a18339766dffe311739d8f8e1f10e54
Sedat MENGÜ <sedatmengu@gmail.com>
2023-03-30 19:05:44 +0300

    Add files via upload

--- StringVeriTipi.py
+++ StringVeriTipi.py
@@ -1,5 +1,9 @@
 name = "HELLO WORLD!"
 
+print("HELLO"+"\n"+"WORLD")
+#print HELLO
+#      WORLD  ( \n ifadesi bir alt satıra geç demektir. \t ifadesi ise bir tab mesafesi ötele demektir.)
+
 
 print(name.capitalize())
 #print = Hello world! (Sadece kelimenin ilk harfi büyük olur)
@@ -18,28 +22,50 @@ print(name.count("L"))
 print(name.encode)
 
 print(name.endswith("!"))
-#print = true (ifadenin parantez içinde belirtilen ile mi bitip bitmediğini kontrol eder. sonuç true / false)
+#print = true (ifadenin parantez içinde belirtilen ile bitip bitmediğini kontrol eder. sonuç true / false)
 
-print(name.expandtabs)
-print(name.find)
-print(name.format)
+print(name.expandtabs())
+
+
+print(name.find("O"))
+#print=4 ( parantez içerisindeki ifadeyi arar ve ilk bulduğu indexi döner. (1.index 0))
+
+
+print("HELLO {} !".format("WORLD"))
+#print= HELLO WORLD (süslü parantez içini format ile de doldurabiliyoruz.)
+
 print(name.format_map)
-print(name.index)
-print(name.isalnum)
-print(name.isalpha)
+
+print(name.index("!"))
+#print = 11 ( parantez içindeki ifadenin hangi indexte olduğunu döner , birden fazla varsa sadece ilkini yakalar)
+
+print(name.isalnum())
+#print = false (ifade tek kelimelik ise ve rakam içeriyorsa true döner , aksi halde false döner)
+
+print(name.isalpha())
+#print = false ( ifade tek kelimelik ise ve harf içeriyorsa true döner , aksi halde false döner)
+
 print(name.isascii)
-print(name.isdecimal)
+print(name.isdecimal())
+#print = false ( ifade decimal ise ture döner , aksi halde false döner)
+
 print(name.isdigit)
 print(name.isidentifier)
-print(name.islower)
+print(name.islower())
+#print= False ( ifadenin bütün harflerinin küçük olup olmadığını kontrol eder. True / False olarak döner)
+
 print(name.isnumeric)
 print(name.isprintable)
 print(name.isspace)
 print(name.istitle)
-print(name.isupper)
+print(name.isupper())
+#print = True ( ifadenin bütün harflerinin büyük olup olmadığını kontrol eder. True / False olarak döner)
+
 print(name.join)
 print(name.ljust)
-print(name.lower)
+print(name.lower())
+#print = hello world! ( ifadenin bütün değerlerinin küçük harfli olmasını sağlar)
+
 print(name.lstrip)
 print(name.maketrans)
 print(name.partition)
@@ -52,14 +78,20 @@ print(name.rjust)
 print(name.rpartition)
 print(name.rsplit)
 print(name.rstrip)
-print(name.split)
+
+print(name.split())
+#print = ['HELLO', 'WORLD!'] ( parantez içindeki ifadeden split etmeye yarar. parantez içinde hangi ifade varsa ordan split eder )
+
 print(name.splitlines)
 print(name.startswith)
 print(name.strip)
 print(name.swapcase)
 print(name.title)
 print(name.translate)
-print(name.upper)
+
+print(name.upper())
+#print = HELLO WORLD! ( ifadenin bütün harflerinin büyük olmasını sağlar)
+
 print(name.zfill)
 
 
